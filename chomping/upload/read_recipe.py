692be0b4171d7729abc16c61884509014ba298f2
bolskie <jbriskie@mailbox.org>
2023-08-07 20:22:09 -0400

    added function to read steps

--- chomping/upload/read_recipe.py
+++ chomping/upload/read_recipe.py
@@ -13,7 +13,7 @@ def read_recipe_file(recipe_name: str) -> dict:
     """read recipe from json file format to dictionary.
 
     :param recipe_name: file name of json recipe, input version 0.01
-    :returns: dictionary of recipe steps
+    :returns: dictionary of recipe data
     """
     # determine path to recipe data
     main_directory = pathlib.Path(__file__).parent.resolve()
@@ -23,6 +23,17 @@ def read_recipe_file(recipe_name: str) -> dict:
 
     # read recipe data from file
     with open(file_path) as json_file:
-        recipe_dict = json.load(json_file)
+        recipe_data = json.load(json_file)
 
-    return recipe_dict
+    return recipe_data
+
+
+def read_recipe_steps(recipe_data: dict) -> list:
+    """read recipe steps and instructions from raw json file data.
+
+    :param recipe_data: dictionary of recipe data returned from read_recipe_file
+    :returns: list of recipe steps and instructions
+    """
+    # read pertinent information from data dictionary, stored natively as list
+    recipe_steps = recipe_data.get("recipe")
+    return recipe_steps
